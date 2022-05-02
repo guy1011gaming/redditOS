83d5be6daa231371a7a03994fde8f66108f3bbcd
Astgut <maxlechner@hotmail.co.uk>
2022-05-02 12:50:40 +0200

    updated getUserPosts Function

--- wrapper/api.py
+++ wrapper/api.py
@@ -6,12 +6,22 @@ user = User('RedditOS_', 'RedditOSMaxHenning2022')
 bot = Bot(user)
 #user.sendMessage('Test', 'Hello fellow user', 'Ill-Union9100')
 
+resp = user.getPosts('new', 'all')
+
+print(len(resp['posts']))
+
+resp = user.getPosts('new', 'all', resp['last_id'])
+
+print(len(resp['posts']))
+
+
 #user.makePost('memes', 'Karma for Karma!', 'Hi, please upvote this post! Have a great day!')
 #resp = bot.findSubreddits('GoneWil', 2)
 #bot.autoPostToMultipleSubreddits(resp, 'Please give me some Karma for a test!', 'Would really appreciate some Karma thanks!!!')
 
-#user.showPosts('hot', 'all', 100)
+#user.getPosts('hot', 'all', 100)
+#bot.showSavedOfUser('controversial', 'all', 10, 'xFregas')
 
-bot.getRedditUsers(['gonewild', 'gonewildcolor', 'gonewildcouples', 'gonewildasian'], ['OF', 'only fans', 'onlyfans'], 100)
+#bot.getRedditUsers(['gonewild', 'gonewildcolor', 'gonewildcouples', 'gonewildasian'], ['OF', 'only fans', 'onlyfans'], 100)
 
 #bot.getCountOfResults('gonewild')
--- wrapper/automate.py
+++ wrapper/automate.py
@@ -1,9 +1,11 @@
 import json
+from queue import Empty
 from reddit_user import User
 import requests
 
 class Bot():
 
+    #Give bot a user account to use
     def __init__(self, user: User):
         self.user = user
         if not self.user.getAuthenticated():
@@ -12,6 +14,7 @@ class Bot():
         else:
             print('User has been automated.')
 
+    #Returns a list of subreddits which were returned by the reddit server with the search term
     def findSubreddits(self, searchTerm: str, amount: int):
         data = {'include_over_18': True, 'include_profiles': False, 'limit': amount, 'query': searchTerm}
         resp = requests.post('https://oauth.reddit.com/api/search_subreddits', data=data, headers=self.user.getHeader())
@@ -29,16 +32,17 @@ class Bot():
             print('An error occurred whilst finding subreddits.')
             return
 
-    def autoPostToMultipleSubreddits(self, collection: list, title: str, text: str):
-        for x in collection:
-            data = {'ad': False, 'api_type': 'json', 'text': text, 'title': title, 'sr': x, 'kind': 'self'}
+    #Pass a collection of subreddits to post a text post to 
+    def autoPostToMultipleSubreddits(self, subreddits: list, title: str, text: str):
+        for subreddit in subreddits:
+            data = {'ad': False, 'api_type': 'json', 'text': text, 'title': title, 'sr': subreddit, 'kind': 'self'}
             
             resp = requests.post('https://oauth.reddit.com/api/submit', headers=self.user.getHeader(), data=data)
 
             if resp.status_code == 200:
-                print('Post to ' + x + ' successful.')
+                print('Post to ' + subreddit + ' successful.')
             else:
-                print('Post to ' + x + ' unsuccessful.')
+                print('Post to ' + subreddit + ' unsuccessful.')
 
     #returns users in a list of subreddits which have any of the given terms in their public description, amount sets how many posts in a subreddit should be returned
     def getRedditUsers(self, subreddits: list, terms: list, amount: int):
@@ -47,31 +51,49 @@ class Bot():
         header = self.user.getHeader()
         for subreddit in subreddits:
             resp = requests.get('https://oauth.reddit.com/r/' + subreddit + '/new', headers=header, data=data, params=param)
-            print(len(resp.json()['data']['children']))
-            try:
-                for k in resp.json()['data']['children']:
-                    if not k['data']['author'] == '[deleted]':
-                        userdata = requests.get('https://oauth.reddit.com/user/' + k['data']['author'] + '/about', headers=self.user.getHeader())
-                        
-                        if userdata.status_code == 200:
-                            marked = False
-                            for term in terms:
-                                if not marked:
-                                    if term.lower() in userdata.json()['data']['subreddit']['public_description'].lower():
-                                        description = (userdata.json()['data']['subreddit']['public_description']).replace('\n', ' ')
-                                        print('u/' + k['data']['author'] + ' says: ' + description)
-                                        marked = True
+            if resp.status_code == 200:
+                print(len(resp.json()['data']['children']))
+                try:
+                    for k in resp.json()['data']['children']:
+                        if not k['data']['author'] == '[deleted]':
+                            userdata = requests.get('https://oauth.reddit.com/user/' + k['data']['author'] + '/about', headers=self.user.getHeader())
+                            
+                            if userdata.status_code == 200:
+                                marked = False
+                                for term in terms:
+                                    if not marked:
+                                        if term.lower() in userdata.json()['data']['subreddit']['public_description'].lower():
+                                            description = (userdata.json()['data']['subreddit']['public_description']).replace('\n', ' ')
+                                            print('u/' + k['data']['author'] + ' says: ' + description)
+                                            marked = True
+                            else:
+                                print('Reddit Server had a problem.')
                         else:
-                            print('Reddit Server had a problem.')
-                    else:
-                        print('User was deleted and could not be found.')
-            except:
-                print('User threw an exception.')
+                            print('User was deleted and could not be found.')
+                except:
+                    print('User threw an exception.')
+            else:
+                print('Error with subreddit.')
 
-    #Testing method for getting how many posts the reddit server sends back upon request
+    #Testing function for getting how many posts the reddit server sends back upon request
     def getCountOfResults(self, subreddit: str):
         header = self.user.getHeader()
         header['limit'] = '100'
         data = {'g': 'GLOBAL', 'show': 'all'}
         resp = requests.get('https://oauth.reddit.com/r/' + subreddit + '/new', headers=header, data=data, params={'limit': 100})
         print(len(resp.json()['data']['children']))
+
+    #Returns posts of given user, sorting, amount and timespan can be defined by parameter. if user is empty it will get posts of own user
+    def showPostsOfUser(self, sort: str, timespan: str, count: int, user = ''):
+        data = {'sort': sort, 't': timespan}
+        param = {'limit': count}
+
+        if user is Empty:
+            user = self.user.getUsername()
+
+        resp = requests.get('https://oauth.reddit.com/user/' + user + '/submitted', headers=self.user.getHeader(), data=data, params=param)
+
+        print(resp)
+        print(sort + ' posts of u/' + user + ':')
+        for post in resp.json()['data']['children']:
+            print('r/' + post['data']['subreddit'] + ':\nTitle: ' + post['data']['title'] + '\nText: ' + post['data']['selftext'] + '\nLinks: ' + post['data']['url'] + '\n\n')
