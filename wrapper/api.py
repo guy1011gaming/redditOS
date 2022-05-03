a573959f8e3282c612f1f41a4b28ca29c7bc6931
Astgut <maxlechner@hotmail.co.uk>
2022-05-03 12:48:57 +0200

    combined automate + reddituser, updated search sub

--- wrapper/api.py
+++ wrapper/api.py
@@ -1,23 +1,30 @@
 from reddit_user import User
-from automate import Bot
 
+def test_login_user():
+    return User('RedditOS_', 'RedditOSMaxHenning2022')
 
-user = User('RedditOS_', 'RedditOSMaxHenning2022')
-bot = Bot(user)
-#user.sendMessage('Test', 'Hello fellow user', 'Ill-Union9100')
+def test_get_user_posts(user: User):
+    resp = {}
+    resp['last_id'] = ''
+    resp['eol'] = False
+    while not resp['eol']:
+        resp = user.getPosts('new', 'all', resp['last_id'])
+    
+def test_send_message(user: User):
+    user.sendMessage('Test', 'Hello fellow user', 'Ill-Union9100')
 
-resp = user.getPosts('new', 'all')
+def test_make_post(user: User):
+    user.makePost('memes', 'Karma for Karma!', 'Hi, please upvote this post! Have a great day!')
 
-print(len(resp['posts']))
+def test_find_subreddits(user: User):
+    return user.findSubreddits('gonewild')
 
-resp = user.getPosts('new', 'all', resp['last_id'])
+def test_auto_post(user: User):
+    user.autoPostToMultipleSubreddits(resp, 'Please give me some Karma for a test!', 'Would really appreciate some Karma thanks!!!')
 
-print(len(resp['posts']))
+user = test_login_user()
 
-
-#user.makePost('memes', 'Karma for Karma!', 'Hi, please upvote this post! Have a great day!')
-#resp = bot.findSubreddits('GoneWil', 2)
-#bot.autoPostToMultipleSubreddits(resp, 'Please give me some Karma for a test!', 'Would really appreciate some Karma thanks!!!')
+test_find_subreddits(user)
 
 #user.getPosts('hot', 'all', 100)
 #bot.showSavedOfUser('controversial', 'all', 10, 'xFregas')
--- wrapper/reddit_user.py
+++ wrapper/reddit_user.py
@@ -77,7 +77,7 @@ class User:
             print('Error whilst sending message.')
             print(resp.json())
 
-    #set a hard limit of 50 posts per call? Which error is returned when last post was reached (handling needed!)?
+    #set a hard limit of 50 posts per call?
     def getPosts(self, sort: str, timespan: str, from_post_id = ''):
         limit = 50
         posts = {}
@@ -86,31 +86,129 @@ class User:
         param = {'limit': limit}
 
         if not from_post_id == '':
-            param['after'] = from_post_id
+            if 't3_' in from_post_id:
+                param['after'] = from_post_id
+            else:
+                param['after'] = 't3_' + from_post_id
         
         try:
             resp = requests.get('https://oauth.reddit.com/user/' + self.username + '/submitted', headers=self.header, data=data, params=param)
 
             if resp.status_code == 200:
                 if len(resp.json()['data']['children']) > 0:
+                    return_values['eol'] = False
                     for post in resp.json()['data']['children']:
                         posts[post['data']['id']] = post
                         return_values['last_id'] = 't3_' + post['data']['id']
                     
                     return_values['posts'] = posts
-                    return return_values
                     #print('User '+ self.username + ' posted in: ' + post['data']['subreddit'] + ', ' + post['data']['selftext'])
                 else:
                     #End of list reached
-                    return None
+                    return_values['eol'] = True
+                    
+                return return_values
             else:
-                pass
+                return None
                 #print('There was an error fetching posts.')
             
         except:
             print('Something went wrong whilst fetching posts...')
             return None
-        
+
+    #Returns a list of subreddits which were returned by the reddit server with the search term
+    def findSubreddits(self, searchTerm: str, from_sub_id = '', over_18_only = True):
+        limit = 50  #change???
+        params = {'q': searchTerm, 'include_over_18': 'on', 'limit': limit, 'show': 'all'}
+
+        if not from_sub_id == '':
+            if 't5_' in from_sub_id:
+                params['after'] = from_sub_id
+            else:
+                params['after'] = 't5_' + from_sub_id
+
+        subreddits = {}
+        return_values = {}
+
+        resp = requests.get('https://oauth.reddit.com/subreddits/search', params=params, headers=self.header)
+
+        print(resp)
+
+        if resp.status_code == 200:
+            if len(resp.json()['data']['children']) > 0:
+                return_values['eol'] = False
+
+                for subreddit in resp.json()['data']['children']:
+                    if (over_18_only and subreddit['data']['over18']) or not over_18_only:
+                        subreddits[subreddit['data']['id']] = subreddit
+                        return_values['last_id'] = 't5_' + subreddit['data']['id']
+                
+                return_values['subreddits'] = subreddits
+            else:
+                return_values['eol'] = True
+
+            return return_values
+        else:
+            print('An error occurred whilst finding subreddits.')
+            return
+
+    #Pass a collection of subreddits to post a text post to 
+    def autoPostToMultipleSubreddits(self, subreddits: list, title: str, text: str):
+        for subreddit in subreddits:
+            data = {'ad': False, 'api_type': 'json', 'text': text, 'title': title, 'sr': subreddit, 'kind': 'self'}
+            #media=media for image?
+            resp = requests.post('https://oauth.reddit.com/api/submit', headers=self.header, data=data)
+
+            if resp.status_code == 200:
+                print('Post to ' + subreddit + ' successful.')
+            else:
+                print('Post to ' + subreddit + ' unsuccessful.')
+
+    #returns users in a list of subreddits which have any of the given terms in their public description, amount sets how many posts in a subreddit should be returned
+    def getRedditUsers(self, subreddits: list, terms: list, amount: int):
+        data = {'g': 'GLOBAL', 'show': 'all'}
+        param = {'limit': amount}
+
+        for subreddit in subreddits:
+            resp = requests.get('https://oauth.reddit.com/r/' + subreddit + '/new', headers=self.header, data=data, params=param)
+            if resp.status_code == 200:
+                print(len(resp.json()['data']['children']))
+                try:
+                    for k in resp.json()['data']['children']:
+                        if not k['data']['author'] == '[deleted]':
+                            userdata = requests.get('https://oauth.reddit.com/user/' + k['data']['author'] + '/about', headers=self.header)
+                            
+                            if userdata.status_code == 200:
+                                marked = False
+                                for term in terms:
+                                    if not marked:
+                                        if term.lower() in userdata.json()['data']['subreddit']['public_description'].lower():
+                                            description = (userdata.json()['data']['subreddit']['public_description']).replace('\n', ' ')
+                                            print('u/' + k['data']['author'] + ' says: ' + description)
+                                            marked = True
+                            else:
+                                print('Reddit Server had a problem.')
+                        else:
+                            print('User was deleted and could not be found.')
+                except:
+                    print('User threw an exception.')
+            else:
+                print('Error with subreddit.')
+
+    #Returns posts of given user, sorting, amount and timespan can be defined by parameter. if user is empty it will get posts of own user
+    def showPostsOfUser(self, sort: str, timespan: str, count: int, user = ''):
+        data = {'sort': sort, 't': timespan}
+        param = {'limit': count}
+
+        if user == '':
+            user = self.user.getUsername()
+
+        resp = requests.get('https://oauth.reddit.com/user/' + user + '/submitted', headers=self.header, data=data, params=param)
+
+        print(resp)
+        print(sort + ' posts of u/' + user + ':')
+        for post in resp.json()['data']['children']:
+            print('r/' + post['data']['subreddit'] + ':\nTitle: ' + post['data']['title'] + '\nText: ' + post['data']['selftext'] + '\nLinks: ' + post['data']['url'] + '\n\n')
 
     def getAuthenticated(self):
         return self.authenticated
